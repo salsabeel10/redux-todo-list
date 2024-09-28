0cd2d068de8c415d8910d5a2faa8f71c4713ff05
salsabeel10 <salsabeelibrahim327@gmail.com>
2024-09-28 14:19:54 +0400

    edit feature added

--- redux/src/Components/List.jsx
+++ redux/src/Components/List.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { MdDeleteForever } from 'react-icons/md'
-import { FaPen } from 'react-icons/fa'
+import { MdDeleteForever, MdEdit } from 'react-icons/md'
 import { useDispatch } from 'react-redux'
-import { removeTodo } from '../features/todo/todoSlice'
-
+import { removeTodo, editTodo } from '../features/todo/todoSlice'
 
 const List = () => {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch()
   const todos = useSelector((state) => state.todos)
+  const [editId, setEditId] = useState(null) // Track which todo is being edited
+  const [newText, setNewText] = useState('') // Store the new text for the todo
+
+  const handleEditClick = (todo) => {
+    setEditId(todo.id) // Set the current todo to edit mode
+    setNewText(todo.text) // Set the input value to the current todo text
+  }
+
+  const handleSaveClick = (id) => {
+    dispatch(editTodo({ id, newText })) // Dispatch edit action with new text
+    setEditId(null) // Exit edit mode
+  }
+
   return (
     <div>
       <ul className="divide-y divide-gray-200 px-4">
@@ -22,15 +33,44 @@ const List = () => {
                   type="checkbox"
                   className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded cursor-pointer"
                 />
-                <label className="ml-3 block text-gray-900">
-                  <span className="text-lg font-medium">{todo.text}</span>
-                </label>
+                {editId === todo.id ? (
+                  // If this todo is being edited, show the input field
+                  <input
+                    type="text"
+                    value={newText}
+                    onChange={(e) => setNewText(e.target.value)}
+                    className="ml-3 text-lg font-medium rounded px-1 outline-none"
+                  />
+                ) : (
+                  // Otherwise, show the todo text
+                  <label className="ml-3 block text-gray-900">
+                    <span className="text-lg font-medium">{todo.text}</span>
+                  </label>
+                )}
               </div>
-              <span 
-              onClick={()=>dispatch(removeTodo(todo.id))}
-              className="text-gray-500 text-2xl cursor-pointer hover:text-red-600">
-                <MdDeleteForever />
-              </span>
+              <div className="flex space-x-1">
+                {editId === todo.id ? (
+                  <button
+                    onClick={() => handleSaveClick(todo.id)}
+                    className="text-blue-600 text-lg cursor-pointer hover:text-blue-700"
+                  >
+                    Save
+                  </button>
+                ) : (
+                  <span
+                    onClick={() => handleEditClick(todo)}
+                    className="text-gray-500 text-2xl cursor-pointer hover:text-blue-600"
+                  >
+                    <MdEdit />
+                  </span>
+                )}
+                <span
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                  className="text-gray-500 text-2xl cursor-pointer hover:text-red-600"
+                >
+                  <MdDeleteForever />
+                </span>
+              </div>
             </div>
           </li>
         ))}
--- redux/src/features/todo/todoSlice.js
+++ redux/src/features/todo/todoSlice.js
@@ -14,24 +14,30 @@ const initialState = {
   ],
 }
 
-export const todoSlice= createSlice({
-    name:'todo',
-    initialState,
-    reducers: {
-        addTodo: (state,action)=>{
-            const todo ={
-                id:nanoid(),
-                text: action.payload,
-            }
-            state.todos.push(todo)
-        },
-        removeTodo: (state,action)=>{
-            state.todos =state.todos.filter((todo)=>
-            todo.id !==action.payload)
-        }
-    }
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      }
+      state.todos.push(todo)
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+    },
+    editTodo: (state, action) => {
+      const { id, newText } = action.payload
+      const todo = state.todos.find((todo) => todo.id === id)
+      if (todo) {
+        todo.text = newText // Update the text of the found todo
+      }
+    },
+  },
 })
  
-export const {addTodo,removeTodo}=todoSlice.actions
+export const {addTodo,removeTodo,editTodo}=todoSlice.actions
 
 export default todoSlice.reducer
